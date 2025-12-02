39926aefa6eedae37b8ae82be16a46f4baa18e60
Sanghuynh83 <thanhsang2288888@gmail.com>
2025-12-02 16:11:39 +0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "baseUrl: "sanghuynh83.github.io/testingweb"",
+    baseUrl: "sanghuynh83.github.io/testingweb",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
